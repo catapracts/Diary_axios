9e639bc78982b82643951c900952223331601900
aa <a@a>
2024-03-25 17:37:18 +0900

    Diary FE(React) & BE(SpringBoot)

---
+++ Front_End_React/Diary-App/src/App.js
@@ -0,0 +1,240 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useReducer, useEffect, useRef, useState } from 'react';
+
+//Routing  처리 라이브러리 import  <== 요청 (/company) ==> 컴포넌트를 연결 
+// Routes, Route  <== Controller 역활 : 요청 ==> View (컴포넌트) 연결 
+// Link , useNavigete : 요청을 보내는 역활 
+import {Routes, Route, Link, useNavigate, useParams} from 'react-router-dom';
+
+import Header from './include/Header';
+import Footer from './include/Footer';
+import Home from './pages/Home';
+import New from './pages/New';
+import Diary from './pages/Diary';
+import Edit from './pages/Edit';
+import ButtonTest from './test/ButtonTest';
+import ImgTest from './test/ImgTest';
+import DiaryService from './Services/DiaryService';
+
+// mockData 가짜 데이터 , 
+const mockData = [
+  {
+    id : 0,
+    date: new Date().getTime() - 1,
+    content:"mock1", 
+    emotionId : 1 
+  },
+  {
+    id : 1,
+    date: new Date().getTime() - 2,
+    content:"mock2", 
+    emotionId : 2 
+  },
+  {
+    id : 2,
+    date: new Date().getTime() - 3,
+    content:"mock3", 
+    emotionId : 3 
+  },
+
+]; 
+
+
+/* 상태 관리 : context , 리덕스, 몹엑스 .... 
+    - context 는 별도의 라이브러리 설치 없이 사용 
+    - SPA 에서 컴포넌트 사이에 상태 (변수의 값) 을 전송 , 
+    - 부모 => 자식  props를 사용해야만 전송 
+
+    1. const DiaryStateContext = React.createContext() 를 사용해서 context 생성 
+    2. Provider 를 사용해서 컴퍼넌트를 묶어 줘야함. , state 를 내려 보내 줄수 있다. 이벤트를 받아올 수 있다. 
+
+    3. useContext : 상태 값을 가져와서 사용할. 
+    4. useReducer 를 사용해서 context 의 상태 값을 변경함. 
+*/
+
+// 1. Context 선언 : 상태 값을 처리 , 이벤트 처리 
+export const DiaryStateContext = React.createContext();   //상태값을 전송 하는 context
+export const DiaryDispatchContext = React.createContext();  // 이벤트를 처리하는 context , 상태값을 변경 
+
+//상태 값을 변경하는 reducer 함수 정의  
+function reducer ( state, action) {
+  switch (action.type) {
+    case "INIT": 
+      return action.data; 
+    case "CREATE":
+      return [action.data, ...state]; 
+    case "DELETE":
+      // state.filter(it) 을 돌려서 ation.targetId !== it.id 을 새로운 배열에 담아서 리턴
+      // id.id 필드의 자료형(Number), action.targetId 필의 자료형 (String)  
+      return state.filter( (it) => String( it.id ) !== String (action.targetId )
+      );
+    case "UPDATE": 
+      return state.map( (it)=> String(it.id) === String(action.data.id) ? 
+                      {...action.data}: it ) ; 
+  }
+}
+
+
+
+function App() {
+
+  const [date, setDate] = useState('');
+  const [content, setContent] = useState('');
+  const [emotionId, setEmotionId] = useState('');
+  const [diary, setDiary] = useState([]);
+  const navigate = useNavigate();
+
+
+  //상태를 처리하는 변수 
+//  const [data , dispatch] = useReducer(reducer,[]); 
+const [data , setData] = useState([]); 
+
+  // useRef Hook을 사용해서 고유한 값을 생성 : id 필드에 적용 
+  const idRef = useRef(3); 
+
+
+  // useEffect 컴포넌트가 로드될때 1번만 실행 
+  // 컴포넌트가 처음 로드될때 dispatch 를 호출해서 data 에 mockData 의 값을 할당. 
+  useEffect( () => {
+    getAllDiary();
+  }
+    ,[]
+  );
+
+  const getAllDiary = () =>
+{
+  DiaryService.getAllDiary().then((response) => 
+  {
+    setData(response.data); 
+    console.log(response.data);
+  }).catch(error => {console.log(error)})
+}
+
+
+  // 하위 컴포넌트에서 요청하는 이벤트 처리 : onCreate, onUpdate, onDelete 
+  // date : yyyy-mm-dd    ===> TimeTemp 형식의 날짜 형식으로 변환 
+  const onCreate = (date, content, emotionId) => {
+
+    // setData({
+    //     type:"CREATE",
+    //     data: {
+    //       id :idRef.current++ , 
+    //       date: new Date(date).getTime(), 
+    //       content:content, 
+    //       emotionId: emotionId ,
+    //     },
+        
+    // });
+    create(date, content, emotionId);
+  }
+
+  const create = (date, content, emotionId) =>
+  {
+    const diary = {date, content, emotionId}
+    DiaryService.createDiary(diary).then((response) => {
+      console.log(response.data)
+    }).catch(error => {console.log(error)})
+  }
+
+
+  const get = (id) =>
+  {
+    DiaryService.getDiaryById(id).then((response) =>
+    {
+      setDate(response.data.date)
+      setContent(response.data.content)
+      setEmotionId(response.data.emotionId)
+    }).catch(error => {console.log(error)})
+  }
+
+
+  const onUpdate = (id, date, emotionId, content) => {
+    console.log (`App 업데이트 날짜 : ${date}`)
+    console.log (`포멧 완료된 날짜 : ${new Date(date).getTime()}`)
+
+    // dispatch({
+    //   type: "UPDATE", 
+    //   data: {
+    //     id : id, 
+    //     date : new Date(date).getTime(),    // yyyy-mm-dd 형식을 TimesTemp 형식으로 변환 
+    //     emotionId : emotionId, 
+    //     content : content,
+    //   }
+    // });
+    update(id, date, emotionId, content);
+    console.log(id)
+    console.log(date)
+    console.log(emotionId)
+    console.log(content)
+  }
+
+
+  const update = (id, date, emotionId, content) =>
+  {
+    const diary = {date, content, emotionId}
+
+    DiaryService.updateDiary(id, diary).then((response) => {
+      console.log(response.data)
+    }).catch(error => {console.log(error)})
+  }
+
+
+  const onDelete = (targetId) => 
+  {
+     // console.log(`하위에서 삭제 id : ${targetId}`)
+//     dispatch({
+//       type:'DELETE',
+//  //     targetId: targetId,     <== 풀어서 사용함. 
+//       targetId,                 // 축약 표현 
+//     });
+
+    deleteD(targetId);
+    console.log(targetId);
+    
+  }
+
+
+  const deleteD = (id) =>
+  {
+   
+    DiaryService.deleteDiary(id).then((response)=>
+    {
+      DiaryService.getAllDiary();
+    }).catch(error => {console.log(error)})
+  }
+
+  return (
+      // 2. context provider를 사용해서 상태를 처리할 하위 컴포넌트를 그룹핑 
+      <DiaryStateContext.Provider value= {data} > 
+      <DiaryDispatchContext.Provider value= {{onCreate, onUpdate, onDelete}}>
+
+    <div className="App">
+        <h1> 다이어리 APP </h1>
+
+      <Header />
+      <hr />
+      <p /><p /><p /><p /><p /><p />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element= {<New />}></Route>
+        <Route path="/diary/:id" element= {<Diary />}> </Route>
+        <Route path="/edit/:id" element= {<Edit />}> </Route>
+
+        <Route path="/btntest" element =  {<ButtonTest />}> </Route>
+        <Route path="/imgtest" elememt = {<ImgTest />}> </Route>
+      </Routes>
+
+      <p /><p /><p /><p /><p /><p />
+      <hr />
+      <Footer /> 
+    </div>
+
+    </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+
+  );
+}
+
+export default App;
---
+++ Front_End_React/Diary-App/src/pages/Home.js
@@ -0,0 +1,75 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Button from '../components/Button';
+import Header from '../components/Header';
+import DiaryList from '../components/DiaryList';
+import { DiaryStateContext } from '../App';
+import { getMonthRangeByDate} from '../utils'; 
+
+function Home(props) {
+
+    // Context provider에서 내려보내주는 상태 값을 불러옴 : useContext  
+    //  data <== mockData       [ {}, {} , {}]
+    //  data : 모든 읽기를 저장하는 배열 
+    const data = useContext(DiaryStateContext); 
+
+    // console.log("id : ", data[0].id); 
+    // console.log("content : ", data[0].content); 
+    // console.log("date : ", data[0].date); 
+    // console.log("emationId : ", data[0].emotionId); 
+
+    //data [{일기1}, {일기2}, {일기3}] 해당월의
+    // stat : 월별로 필터링된 일기 배열을 저장 
+    const [filteredDate, setfilteredDate] = useState([]); 
+
+     //현재 년월을 출력하는 useState 
+    const [pivotDate, setPivotDate] = useState( new Date()); 
+
+    // useEffect 를 사용해서 Home 컴포넌트가 랜더링될때, [data,pivotDate] 가 수정이 될때 함수 랜더링
+    useEffect(
+        () => {
+           if (data.length >= 1 ) {        
+            // 현재 년월에 대한 시작일, 종료일을 가지고 옮
+            const { beginTimeStemp, endTimeStemp} = getMonthRangeByDate(pivotDate); 
+
+            // data : 전체 읽기에 대한 내용 ===> 해당월의 일기 내용만 필더 해서 새로운 배열에 저장 
+            setfilteredDate (
+                data.filter(
+                    (it) => beginTimeStemp <= it.date && it.date <= endTimeStemp
+                ) 
+            ); 
+           } 
+        } , [data, pivotDate]
+    ); 
+
+
+
+
+    const headerTitle = `${pivotDate.getFullYear()} 년 ${pivotDate.getMonth() + 1 }월` 
+
+    const onDecreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1 ) ); 
+    }
+    const onIncreseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1 ) ); 
+    }
+
+
+    return (
+        <div>
+            <Header 
+                title = {headerTitle} 
+                leftChild={<Button text={" < "} type="positive" 
+                    onClick={ onDecreaseMonth } />}
+                rightChild={<Button text={" > "} type="negative" 
+                    onClick={ onIncreseMonth }/>}
+            />
+
+            <DiaryList data={filteredDate} />
+{/* 
+            <DiaryList data={data} />
+ */}
+        </div>
+    );
+}
+
+export default Home;
